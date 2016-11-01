05d3869329522a053a0a34d236590303152072d3
waixiaoyu <yaoyu_best@163.com>
2016-11-01 00:52:29 -0400

    assignment 2 part B1

--- src/simpledb/AbstractDbIterator.java
+++ src/simpledb/AbstractDbIterator.java
@@ -2,21 +2,24 @@ package simpledb;
 
 import java.util.NoSuchElementException;
 
-/** Helper for implementing DbIterators. Handles hasNext()/next() logic and
- *  throwing exceptions if open()/close() are abused. */
+/**
+ * Helper for implementing DbIterators. Handles hasNext()/next() logic and
+ * throwing exceptions if open()/close() are abused.
+ */
 public abstract class AbstractDbIterator implements DbIterator {
     //    @Override
     public boolean hasNext() throws DbException, TransactionAbortedException {
-        if (next == null) next = readNext();
+        if (next == null)
+            next = readNext();
         return next != null;
     }
 
     //    @Override
-    public Tuple next() throws
-            DbException, TransactionAbortedException, NoSuchElementException {
+    public Tuple next() throws DbException, TransactionAbortedException, NoSuchElementException {
         if (next == null) {
             next = readNext();
-            if (next == null) throw new NoSuchElementException();
+            if (next == null)
+                throw new NoSuchElementException();
         }
 
         Tuple result = next;
@@ -24,7 +27,10 @@ public abstract class AbstractDbIterator implements DbIterator {
         return result;
     }
 
-    /** @return the next Tuple in the iterator, null if the iteration is finished. */
+    /**
+     * @return the next Tuple in the iterator, null if the iteration is
+     *         finished.
+     */
     protected abstract Tuple readNext() throws DbException, TransactionAbortedException;
 
     /** If subclasses override this, they should call super.close(). */
--- test/simpledb/systemtest/JoinTest.java
+++ test/simpledb/systemtest/JoinTest.java
@@ -8,28 +8,24 @@ import org.junit.Test;
 
 import simpledb.*;
 
-       
-
 public class JoinTest {
     private static final int COLUMNS = 4;
-    public void validateJoin(int table1ColumnValue, int table1Rows, int table2ColumnValue,
-            int table2Rows)
+
+    public void validateJoin(int table1ColumnValue, int table1Rows, int table2ColumnValue, int table2Rows)
             throws IOException, DbException, TransactionAbortedException {
         // Create the two tables
         HashMap<Integer, Integer> columnSpecification = new HashMap<Integer, Integer>();
         columnSpecification.put(0, table1ColumnValue);
         ArrayList<ArrayList<Integer>> t1Tuples = new ArrayList<ArrayList<Integer>>();
-        HeapFile table1 = SystemTestUtil.createRandomHeapFile(
-                COLUMNS, table1Rows, columnSpecification, t1Tuples);
+        HeapFile table1 = SystemTestUtil.createRandomHeapFile(COLUMNS, table1Rows, columnSpecification, t1Tuples);
         assert t1Tuples.size() == table1Rows;
-	System.out.println("JoinTest Relation 1 is  "+ table1.numPages() +"  pages");
+        System.out.println("JoinTest Relation 1 is  " + table1.numPages() + "  pages");
 
         columnSpecification.put(0, table2ColumnValue);
         ArrayList<ArrayList<Integer>> t2Tuples = new ArrayList<ArrayList<Integer>>();
-        HeapFile table2 = SystemTestUtil.createRandomHeapFile(
-                COLUMNS, table2Rows, columnSpecification, t2Tuples);
+        HeapFile table2 = SystemTestUtil.createRandomHeapFile(COLUMNS, table2Rows, columnSpecification, t2Tuples);
         assert t2Tuples.size() == table2Rows;
-	System.out.println("JoinTest Relation 2 is  "+ table2.numPages() +"  pages");
+        System.out.println("JoinTest Relation 2 is  " + table2.numPages() + "  pages");
 
         // Generate the expected results
         ArrayList<ArrayList<Integer>> expectedResults = new ArrayList<ArrayList<Integer>>();
@@ -50,7 +46,7 @@ public class JoinTest {
         SeqScan ss2 = new SeqScan(tid, table2.id(), "");
         JoinPredicate p = new JoinPredicate(0, Predicate.Op.EQUALS, 0);
         Join joinOp = new Join(p, ss1, ss2);
-	joinOp.setJoinAlgorithm(Join.SNL);
+        joinOp.setJoinAlgorithm(Join.SNL);
 
         // test the join results
         SystemTestUtil.matchTuples(joinOp, expectedResults);
@@ -58,32 +54,27 @@ public class JoinTest {
         joinOp.close();
         Database.getBufferPool().transactionComplete(tid);
         Database.getBufferPool().flushAllPages();
-	System.out.println("Outer Relation: "+ss1.getPagesRead()+" pages read");
-	System.out.println("Inner Relation: "+ss2.getPagesRead()+" pages read");
-	System.out.println("Number of Joined Tuples: "+joinOp.getNumMatches());
+        System.out.println("Outer Relation: " + ss1.getPagesRead() + " pages read");
+        System.out.println("Inner Relation: " + ss2.getPagesRead() + " pages read");
+        System.out.println("Number of Joined Tuples: " + joinOp.getNumMatches());
     }
 
     //runs the join but does not check the results (to save memory)
-    public void performJoin(int table1ColumnValue, int table1Rows, int table2ColumnValue,
-            int table2Rows)
+    public void performJoin(int table1ColumnValue, int table1Rows, int table2ColumnValue, int table2Rows)
             throws IOException, DbException, TransactionAbortedException {
         // Create the two tables
         HashMap<Integer, Integer> columnSpecification = new HashMap<Integer, Integer>();
         columnSpecification.put(0, table1ColumnValue);
         ArrayList<ArrayList<Integer>> t1Tuples = new ArrayList<ArrayList<Integer>>();
-        HeapFile table1 = SystemTestUtil.createRandomHeapFile(
-                COLUMNS, table1Rows, columnSpecification, t1Tuples);
+        HeapFile table1 = SystemTestUtil.createRandomHeapFile(COLUMNS, table1Rows, columnSpecification, t1Tuples);
         assert t1Tuples.size() == table1Rows;
-	System.out.println("JoinTest Relation 1 is  "+ table1.numPages() +"  pages");
+        System.out.println("JoinTest Relation 1 is  " + table1.numPages() + "  pages");
 
         columnSpecification.put(0, table2ColumnValue);
         ArrayList<ArrayList<Integer>> t2Tuples = new ArrayList<ArrayList<Integer>>();
-        HeapFile table2 = SystemTestUtil.createRandomHeapFile(
-                COLUMNS, table2Rows, columnSpecification, t2Tuples);
+        HeapFile table2 = SystemTestUtil.createRandomHeapFile(COLUMNS, table2Rows, columnSpecification, t2Tuples);
         assert t2Tuples.size() == table2Rows;
-	System.out.println("JoinTest Relation 2 is  "+ table2.numPages() +"  pages");
-
-    
+        System.out.println("JoinTest Relation 2 is  " + table2.numPages() + "  pages");
 
         // Begin the join
         TransactionId tid = new TransactionId();
@@ -91,7 +82,7 @@ public class JoinTest {
         SeqScan ss2 = new SeqScan(tid, table2.id(), "");
         JoinPredicate p = new JoinPredicate(0, Predicate.Op.EQUALS, 0);
         Join joinOp = new Join(p, ss1, ss2);
-	joinOp.setJoinAlgorithm(Join.SNL);
+        joinOp.setJoinAlgorithm(Join.SNL);
 
         // create and drop the join results
         SystemTestUtil.countJoinTuples(joinOp);
@@ -99,55 +90,53 @@ public class JoinTest {
         joinOp.close();
         Database.getBufferPool().transactionComplete(tid);
         Database.getBufferPool().flushAllPages();
-	System.out.println("Outer Relation: "+ss1.getPagesRead()+" pages read");
-	System.out.println("Inner Relation: "+ss2.getPagesRead()+" pages read");
-	System.out.println("Number of Joined Tuples: "+joinOp.getNumMatches());
+        System.out.println("Outer Relation: " + ss1.getPagesRead() + " pages read");
+        System.out.println("Inner Relation: " + ss2.getPagesRead() + " pages read");
+        System.out.println("Number of Joined Tuples: " + joinOp.getNumMatches());
 
     }
 
-    @Test public void testSingleMatch()
-            throws IOException, DbException, TransactionAbortedException {
-	System.out.println("--------------------------------------------");
-	System.out.println("JoinTest - Test Single Match");
+    @Test
+    public void testSingleMatch() throws IOException, DbException, TransactionAbortedException {
+        System.out.println("--------------------------------------------");
+        System.out.println("JoinTest - Test Single Match");
         validateJoin(1, 1, 1, 1);
     }
 
-    @Test public void testNoMatch()
-            throws IOException, DbException, TransactionAbortedException {
-	System.out.println("--------------------------------------------");
-	System.out.println("JoinTest - Test No Match");
+    @Test
+    public void testNoMatch() throws IOException, DbException, TransactionAbortedException {
+        System.out.println("--------------------------------------------");
+        System.out.println("JoinTest - Test No Match");
         validateJoin(1, 2, 2, 10);
     }
 
-    @Test public void testMultipleMatch()
-            throws IOException, DbException, TransactionAbortedException {
-	System.out.println("--------------------------------------------");
-	System.out.println("JoinTest - Test Multiple Matches");
+    @Test
+    public void testMultipleMatch() throws IOException, DbException, TransactionAbortedException {
+        System.out.println("--------------------------------------------");
+        System.out.println("JoinTest - Test Multiple Matches");
         validateJoin(1, 11, 1, 22);
     }
 
-    @Test public void testLargeOuter()
-          throws IOException, DbException, TransactionAbortedException {
-    	System.out.println("--------------------------------------------");
-    	System.out.println("JoinTest - Test Large Outer");
-	performJoin(1, 700, 1, 1);
+    @Test
+    public void testLargeOuter() throws IOException, DbException, TransactionAbortedException {
+        System.out.println("--------------------------------------------");
+        System.out.println("JoinTest - Test Large Outer");
+        performJoin(1, 700, 1, 1);
     }
 
-
-    @Test public void testLargeInner()
-        throws IOException, DbException, TransactionAbortedException {
-    	System.out.println("--------------------------------------------");
-    	System.out.println("JoinTest - Test Large Inner");
-	performJoin(1, 1, 1, 700);
+    @Test
+    public void testLargeInner() throws IOException, DbException, TransactionAbortedException {
+        System.out.println("--------------------------------------------");
+        System.out.println("JoinTest - Test Large Inner");
+        performJoin(1, 1, 1, 700);
     }
 
-    @Test public void testLargeOuterInner()
-          throws IOException, DbException, TransactionAbortedException {
-    	System.out.println("--------------------------------------------");
-    	System.out.println("JoinTest - Test Large Outer and Inter");
-	performJoin(1, 500, 1, 600);
+    @Test
+    public void testLargeOuterInner() throws IOException, DbException, TransactionAbortedException {
+        System.out.println("--------------------------------------------");
+        System.out.println("JoinTest - Test Large Outer and Inter");
+        performJoin(1, 500, 1, 600);
     }
-
 
     /** Make test compatible with older version of ant. */
     public static junit.framework.Test suite() {
